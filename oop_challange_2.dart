ba51e955a4646138abe480a7848a7db5c09cb732
bivomuh <bmuhandeza@gmail.com>
2023-05-07 23:23:20 +0700

    revise oop_challange_2

--- oop_challange_2.dart
+++ oop_challange_2.dart
@@ -5,7 +5,13 @@ class Mahasiswa {
   String? jurusan;
   int? angkatan;
 
-  Mahasiswa(this.nama, this.jurusan, this.angkatan);
+  Mahasiswa() {
+    print("this is default constructor");
+  }
+  Mahasiswa.parameterizedCons(this.nama, this.jurusan, this.angkatan) {
+    print("this is parameterized constructor");
+  }
+
   Mahasiswa.fromJson(String jsonString) {
     Map<String, dynamic> json = jsonDecode(jsonString);
     nama = json['nama'];
@@ -15,6 +21,12 @@ class Mahasiswa {
 }
 
 void main() {
+  Mahasiswa mahasiswa0 = Mahasiswa();
+  print(mahasiswa0);
+
+  Mahasiswa mahasiswa1 = Mahasiswa.parameterizedCons("muh", "kedokteran", 2017);
+  print(mahasiswa1);
+
   String jsonString1 =
       '{"nama" : "bivo", "jurusan" : "teknik elektro", "angkatan" : 2017}';
   Mahasiswa mahasiswa = Mahasiswa.fromJson(jsonString1);
